c609b2b1235d466b8c139163865106d092e6ef68
thomasbrettell <thomas.a.brettell@gmail.com>
2022-01-16 17:03:49 +1100

    begin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import './styles.scss';
+import {
+  Scene,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  PerspectiveCamera,
+  WebGLRenderer,
+} from 'three';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//setup
+const WINDOW_WIDTH = 800;
+const WINDOW_HEIGHT = 600;
+const canvasEl = document.getElementById('canvas');
+
+//scene
+const scene = new Scene();
+
+//cube
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({
+  color: 'red',
+});
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+//camera
+const camera = new PerspectiveCamera(75, WINDOW_WIDTH / WINDOW_HEIGHT);
+camera.position.z = 3
+camera.position.x = 1
+scene.add(camera);
+
+//renderer
+const renderer = new WebGLRenderer({
+  canvas: canvasEl,
+});
+renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+renderer.render(scene, camera);
